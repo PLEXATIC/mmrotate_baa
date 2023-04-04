5870c4dd18dc1167a94920923359b841c64e2fed
PLEXATIC <plexatic@gmail.com>
2023-04-04 20:48:10 +0200

    video generator

--- demo/video_generator.py
+++ demo/video_generator.py
@@ -1,25 +1,25 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from argparse import ArgumentParser
 import os
-from mmdet.apis import inference_detector, init_detector, show_result_pyplot
+import mmcv
+from mmdet.apis import inference_detector, init_detector
 
-import mmrotate  # noqa: F401
+from mmrotate.registry import VISUALIZERS
+from mmrotate.utils import register_all_modules
 
 
 def parse_args():
     parser = ArgumentParser()
-    parser.add_argument('img_folder', help='Image file')
-    parser.add_argument('start_index', help='start_index of video')
-    parser.add_argument('num_frames', help='number of video frames')
+    parser.add_argument('img', help='Image directory')
     parser.add_argument('config', help='Config file')
     parser.add_argument('checkpoint', help='Checkpoint file')
-    parser.add_argument('--out-file', default=None, help='Path to output file')
+    parser.add_argument('--out-folder', default=None, help='Path to output file')
     parser.add_argument(
         '--device', default='cuda:0', help='Device used for inference')
     parser.add_argument(
         '--palette',
         default='dota',
-        choices=['dota', 'sar', 'hrsc', 'hrsc_classwise', 'random'],
+        choices=['dota', 'sar', 'hrsc', 'random'],
         help='Color palette used for visualization')
     parser.add_argument(
         '--score-thr', type=float, default=0.3, help='bbox score threshold')
@@ -28,23 +28,35 @@ def parse_args():
 
 
 def main(args):
-    # build the model from a config file and a checkpoint file
-    model = init_detector(args.config, args.checkpoint, device=args.device)
-    # test a single image
+    # register all modules in mmrotate into the registries
+    register_all_modules()
 
-    image_paths = os.listdir(args.img_folder)
-    for i in range(int(args.start_index), int(args.start_index)+int(args.num_frames),1):
-        img_path = os.path.join(args.img_folder, image_paths[i])
-        result = inference_detector(model, img_path)
-        out_path = args.out_file + f"_{i}.png"
-        # show the results
-        show_result_pyplot(
-            model,
-            img_path,
-            result,
-            palette=args.palette,
-            score_thr=args.score_thr,
-            out_file=out_path)
+    # build the model from a config file and a checkpoint file
+    model = init_detector(
+        args.config, args.checkpoint, palette=args.palette, device=args.device)
+
+    # init visualizer
+    visualizer = VISUALIZERS.build(model.cfg.visualizer)
+    # the dataset_meta is loaded from the checkpoint and
+    # then pass to the model in init_detector
+    visualizer.dataset_meta = model.dataset_meta
+
+    # test a single image
+    for image in os.listdir(args.img):
+        image_path = os.path.join(args.img, image)
+        result = inference_detector(model, image_path)
+        img = mmcv.imread(image_path)
+        result_path = os.path.join(args.out_folder, image)
+        img = mmcv.imconvert(img, 'bgr', 'rgb')
+        visualizer.add_datasample(
+            'result',
+            img,
+            data_sample=result,
+            draw_gt=True,
+            show=args.out_folder is None,
+            wait_time=0,
+            out_file=result_path,
+            pred_score_thr=args.score_thr)
 
 if __name__ == '__main__':
     args = parse_args()
